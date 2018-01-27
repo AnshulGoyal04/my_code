e0b85bde5d3558041f1cce8ae7613753056b1e05
AnshulGoyal04 <35332655+AnshulGoyal04@users.noreply.github.com>
2018-01-27 14:01:13 +0530

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int y;
+	scanf("%d",&y);
+	if (y% 400 == 0)
+        printf("Yes");
+    else if (y% 100 == 0)
+        printf("No");
+    else if (y% 4 == 0)
+        printf("Yes");
+    else
+		printf("No");
+}
